0475fe89fa7217165558a5acdc2352d8c8336b8e
Cal Stephens <cal@calstephens.tech>
2016-09-24 01:39:21 -0400

    Start with a basic watch app & empty framework target

---
+++ WatchKit for iOS/WatchKit for iOS.h
@@ -0,0 +1,19 @@
+//
+//  WatchKit for iOS.h
+//  WatchKit for iOS
+//
+//  Created by Cal Stephens on 9/24/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchKit for iOS.
+FOUNDATION_EXPORT double WatchKit_for_iOSVersionNumber;
+
+//! Project version string for WatchKit for iOS.
+FOUNDATION_EXPORT const unsigned char WatchKit_for_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchKit_for_iOS/PublicHeader.h>
+
+
---
+++ WatchKit for iOS/WatchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WatchViewController.swift
+//  Basic Watch App
+//
+//  Created by Cal Stephens on 9/24/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+import UIKit
+
+public class WatchViewController : UIViewController {
+    
+    public init(watchStoryboardName: String) {
+        print(watchStoryboardName)
+    }
+    
+}
